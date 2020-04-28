579ad2ab662f0995b013225b6aa0c9785cf1558c
roughwin <roughwin@users.noreply.github.com>
2020-04-28 17:11:52 +0000

    deploy: 1b5e544f375f3a923749e374707d71c883b6ebc4

---
+++ precache-manifest.11dcad0532dddbdb1fc4f0899aa14f6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b4b0c9327f4cc6e45d9cae8dc7f8f4",
+    "url": "/clipboard-demo/index.html"
+  },
+  {
+    "revision": "cfa60f64200471e40ac2",
+    "url": "/clipboard-demo/static/js/2.85080b73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clipboard-demo/static/js/2.85080b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4081dced045391644ec",
+    "url": "/clipboard-demo/static/js/main.83e09d12.chunk.js"
+  },
+  {
+    "revision": "07e10c93747fb6fdf012",
+    "url": "/clipboard-demo/static/js/runtime-main.69949359.js"
+  }
+]);
